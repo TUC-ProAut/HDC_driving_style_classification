23b8e5423a8ae3d05651b152a18fed40767a6ff3
Kenny <scken@tuc>
2021-05-20 09:19:57 +0200

    added function to create HDC vectors with tensorflow

--- utils.py
+++ utils.py
@@ -3,17 +3,17 @@ import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 import os
-import cmath
 import tensorflow as tf
-from joblib import Parallel, delayed
-import multiprocessing
+from tensorflow.python.client import timeline
 import io
 from scipy.io import savemat, loadmat
-from keras.callbacks import Callback
-from time import time
-from keras.models import Model
-from keras.layers import Input, Dense, LSTM, multiply, concatenate, Activation, Masking, Reshape
-from keras.layers import Conv1D, BatchNormalization, GlobalAveragePooling1D, Permute, Dropout
+from tensorflow.keras.callbacks import Callback
+import time
+from tensorflow.keras.layers import Input, Dense, LSTM, multiply, concatenate, Activation, Masking, Reshape
+from tensorflow.keras.layers import Conv1D, BatchNormalization, GlobalAveragePooling1D, Permute, Dropout
+from tensorflow.python.eager.context import context, EAGER_MODE, GRAPH_MODE, eager_mode, graph_mode
+
+from model import HDC_tf_preproc
 
 def extract_batch_size(_train, step, batch_size):
     # Function to fetch a "batch_size" amount of data from "(X|y)_train" data. 
@@ -103,28 +103,12 @@ class TimingCallback(Callback):
     def __init__(self):
         self.logs=[]
     def on_epoch_begin(self,epoch, logs={}):
-        self.starttime=time()
+        self.starttime=time.time()
     def on_epoch_end(self, epoch, logs={}):
-        self.logs.append(time()-self.starttime)
+        self.logs.append(time.time()-self.starttime)
 
-def squeeze_excite_block(input):
-    ''' Create a squeeze-excite block
-    Args:
-        input: input tensor
-        filters: number of output filters
-        k: width factor
-    Returns: a keras tensor
-    '''
-    filters = input.shape[-1] # channel_axis = -1 for TF
 
-    se = GlobalAveragePooling1D()(input)
-    se = Reshape((1, filters))(se)
-    se = Dense(filters // 16,  activation='relu', kernel_initializer='he_normal', use_bias=False)(se)
-    se = Dense(filters, activation='sigmoid', kernel_initializer='he_normal', use_bias=False)(se)
-    se = multiply([input, se])
-    return se
-
-def load_dataset(dataset,config,hdc_encoded):
+def load_dataset(dataset,config):
     """
     load the specific data set (from the data/ folder)
     @param dataset: specifies the data set [string]
@@ -134,54 +118,17 @@ def load_dataset(dataset,config,hdc_encoded):
     """
     # load preprocessed data
     if dataset == "full":
-        if hdc_encoded==True:
-            try:
-                data = loadmat('data/preproc_data' + '_' + str(config.input_dim) + '_' + str(config.scale) + '.mat')
-                X_train, X_test, y_train, y_test = train_test_split(
-                    np.concatenate((data['output_motor'], data['output_second']),
-                                   axis=0),
-                    np.concatenate((data['motorway_labels'], data['secondary_labels']),
-                                   axis=0), test_size=0.33, random_state=42)
-            except:
-                print('The dataset could not be loaded and may not have been preprocessed.')
-        else:
-            X_train, X_test, y_train, y_test = load_full_dataset()
+        X_train, X_test, y_train, y_test = load_full_dataset()
     elif dataset == "motorway":
-        if hdc_encoded==True:
-            try:
-                data = loadmat('data/preproc_data' + '_' + str(config.input_dim) + '_' + str(config.scale) + '.mat')
-                X_train, X_test, y_train, y_test = train_test_split(data['output_motor'], data['motorway_labels'],
-                                                                    test_size=0.33, random_state=42)
-            except:
-                print('The dataset could not be loaded and may not have been preprocessed.')
-        else:
-            X_train, X_test, y_train, y_test = load_motorway_dataset()
+        X_train, X_test, y_train, y_test = load_motorway_dataset()
     elif dataset == "secondary":
-        if hdc_encoded==True:
-            try:
-                data = loadmat('data/preproc_data' + '_' + str(config.input_dim) + '_' + str(config.scale) + '.mat')
-                X_train, X_test, y_train, y_test = train_test_split(data['output_second'], data['secondary_labels'],
-                                                                    test_size=0.33, random_state=42)
-            except:
-                print('The dataset could not be loaded and may not have been preprocessed.')
-        else:
-            X_train, X_test, y_train, y_test = load_secondary_dataset()
+        X_train, X_test, y_train, y_test = load_secondary_dataset()
     elif dataset == "full_crossval":
-        if hdc_encoded==True:
-            try:
-                data = loadmat('data/preproc_data' + '_' + str(config.input_dim) + '_' + str(config.scale) + '.mat')
-            except:
-                print('The dataset could not be loaded and may not have been preprocessed.')
-            mot_data = data['output_motor']
-            sec_data = data['output_second']
-            mot_label = data['motorway_labels']
-            sec_label = data['secondary_labels']
-        else:
-            data = loadmat('data/uah_dataset.mat')
-            mot_data = data['motorway_dataset']
-            sec_data = data['secondary_dataset']
-            mot_label = data['motorway_labels']
-            sec_label = data['secondary_labels']
+        data = loadmat('data/uah_dataset.mat')
+        mot_data = data['motorway_dataset']
+        sec_data = data['secondary_dataset']
+        mot_label = data['motorway_labels']
+        sec_label = data['secondary_labels']
 
         # creat cross-validation split (3-fold cross-validation) on the full dataset
         k = 3
@@ -206,8 +153,46 @@ def load_dataset(dataset,config,hdc_encoded):
             X_test[k_idx]=np.concatenate((X_test[k_idx],sec_data[test_idx]),0)
             y_test[k_idx]=np.concatenate((y_test[k_idx],sec_label[test_idx]), 0)
             k_idx+=1
-
     else:
         print("No valid dataset argument was set!")
 
     return X_train, X_test, y_train, y_test, config
+
+
+def create_HDC_vectors(config, input):
+    """
+    create the HDC vectors from given input
+    @param config: config struct
+    @param input: inputs tensor with size m x t x v (m... number of samples, t... number of timesteps, v... number of variables)
+    """
+    with graph_mode():
+        tf.config.optimizer.set_jit(True)
+        # pre initialize vectors
+        init_vec = tf.random.uniform(shape=(config.input_dim, 1), minval=-np.pi, maxval=np.pi, seed=1,
+                                     dtype="float32")
+        sensor_ids = tf.random.uniform(shape=(config.input_dim, config.n_inputs), minval=-np.pi, maxval=np.pi,
+                                       seed=2,
+                                       dtype="float32")
+        timestamps = tf.random.uniform(shape=(config.input_dim, config.n_steps), minval=-np.pi, maxval=np.pi,
+                                       seed=3,
+                                       dtype="float32")
+        init_vecs = {'init_vec': init_vec, 'sensor_ids': sensor_ids, 'timestamps': timestamps, 'scale':config.scale}
+
+        X = tf.compat.v1.placeholder(tf.float32, [None, config.n_steps, config.n_inputs], name="X")
+        preproc = HDC_tf_preproc(X, init_vecs)
+        t_proc = []
+        traces = []
+
+        for i in range(config.n_time_measures):
+            sess = tf.compat.v1.Session()
+            options = tf.compat.v1.RunOptions(trace_level=tf.compat.v1.RunOptions.FULL_TRACE)
+            run_metadata = tf.compat.v1.RunMetadata()
+            t = time.perf_counter()
+            output = sess.run(preproc, feed_dict={X: input}, options=options, run_metadata=run_metadata)
+            t_proc.append((time.perf_counter() - t))
+            fetched_timeline = timeline.Timeline(run_metadata.step_stats)
+            chrome_trace = fetched_timeline.generate_chrome_trace_format()
+            traces.append(chrome_trace)
+        preprocessing_time = np.median(t_proc)
+
+    return preprocessing_time, output, traces, init_vecs
